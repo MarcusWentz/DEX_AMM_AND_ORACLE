224d9ef0276a145fe178cd50fee246fa6d7d7d2c
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-02-06 00:30:38 -0500

    Update MaticPolygonAMMDEX.sol

--- Contracts/MaticPolygonAMMDEX.sol
+++ Contracts/MaticPolygonAMMDEX.sol
@@ -1,17 +1,18 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.11;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol';
 
 contract ERC20TokenContract is ERC20('Chainlink', 'LINK') {}
 
 contract swapPoolMATICLINK{
-    
+
     uint public constantProduct;
     uint public contractMATICBalance;
     uint public contractLINKBalance;
-    address public ChainlinkTokenAddressMatic = 0x326C977E6efc84E512bB9C30f76E30c160eD06FB;
-    ERC20TokenContract tokenObject = ERC20TokenContract(ChainlinkTokenAddressMatic);
+    address public ChainlinkTokenAddressRinkeby = 0x326C977E6efc84E512bB9C30f76E30c160eD06FB;
+    ERC20TokenContract tokenObject = ERC20TokenContract(ChainlinkTokenAddressRinkeby);
     address public immutable Owner;
 
     constructor() {
@@ -34,7 +35,7 @@ contract swapPoolMATICLINK{
         contractLINKBalance = tokenObject.balanceOf(address(this));
         constantProduct = contractMATICBalance*contractLINKBalance;
     }
-    
+
     function step2_swapMATICforLINK() public payable {
          require(contractLINKBalance == 4 && contractMATICBalance == 4, "Must have 4 MATIC and 4 LINK in the contract to do this.");
          require(msg.value == (((constantProduct)/(contractLINKBalance-2))-contractMATICBalance) , "You need to put 4 MATIC in the value section to do this."); // 4 MATIC from user to contract
@@ -42,24 +43,24 @@ contract swapPoolMATICLINK{
          contractMATICBalance = address(this).balance;
          contractLINKBalance = tokenObject.balanceOf(address(this));
     }
-    
+
     //NEED TO APPROVE EVERY TIME BEFORE YOU SEND LINK FROM THE ERC20 CONTRACT!
     function step3_swapLINKforMATIC() public {
         require(contractLINKBalance == 2 && contractMATICBalance == 8, "Must have 8 MATIC and 2 LINK in the contract to do this.");
         require(tokenObject.balanceOf(address(msg.sender)) >= ((constantProduct)/(contractMATICBalance- 4)) - contractLINKBalance  , "You need at least 2 LINK in your account to do this.");
         require(tokenObject.allowance(msg.sender,address(this)) >= ((constantProduct)/(contractMATICBalance- 4)) - contractLINKBalance  , "Must allow 2 tokens from your wallet in the ERC20 contract!");
         tokenObject.transferFrom(msg.sender, address(this), ((constantProduct)/(contractMATICBalance- 4)) - contractLINKBalance  ); // 2 LINK from user to contract
+        payable(msg.sender).transfer(4); // 4 MATIC from contract to user
         contractMATICBalance = address(this).balance;
         contractLINKBalance = tokenObject.balanceOf(address(this));
-        payable(msg.sender).transfer(4); // 4 MATIC from contract to user
     }    
 
     function WithdrawAllLINKAndMATIC() public LiquidityProviderAddressCheck  {
+         payable(Owner).transfer(contractMATICBalance);
          tokenObject.transfer(Owner, contractLINKBalance);
          constantProduct = 0;
          contractMATICBalance = 0;
          contractLINKBalance = 0;
-         payable(Owner).transfer(address(this).balance);
     }
 
 }
