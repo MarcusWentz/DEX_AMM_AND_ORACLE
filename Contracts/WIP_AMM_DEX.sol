47055571f17d1263639b8eaa7ac3330a1c9a39f5
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-07-06 21:43:54 -0400

    Update WIP_AMM_DEX.sol

--- Contracts/WIP_AMM_DEX.sol
+++ Contracts/WIP_AMM_DEX.sol
@@ -8,7 +8,6 @@ contract ERC20TokenContract is ERC20('Chainlink', 'LINK') {}
 contract swapPoolMATICLINK {
     
     address public immutable Owner;
-    uint public testValue;
     
     // ERC20TokenContract tokenObject = ERC20TokenContract(0x326C977E6efc84E512bB9C30f76E30c160eD06FB); //Chainlink contract address on Polygon testnet.
     ERC20TokenContract tokenObject = ERC20TokenContract(0xfe4F5145f6e09952a5ba9e956ED0C25e3Fa4c7F1); //ERC20 contract address on Polygon testnet.
@@ -39,12 +38,12 @@ contract swapPoolMATICLINK {
     }
 
     function swapMATICforLINK() public payable poolExists {
-        require(maticforLinkBalanced(0) );
-        tokenObject.transfer(msg.sender, linkToReceive(0) ); 
+        // require(maticforLinkBalanced(msg.value), "Pool not balanced!");
+        tokenObject.transfer(msg.sender, linkToReceiveMaticReceived(msg.value) ); 
     }
     
     function swapLINKforMATIC(uint payLink) public poolExists {     //NEED TO APPROVE EVERY TIME BEFORE YOU SEND LINK FROM THE ERC20 CONTRACT!
-        require(payLink*maticToReceive(payLink) == poolConstantProduct(), "Pool not balanced!");
+        // require(linkforMaticBalanced(payLink), "Pool not balanced!");
         uint memoryMaticPayout = maticToReceive(payLink);
         tokenObject.transferFrom(msg.sender, address(this),  payLink ); 
         payable(msg.sender).transfer(memoryMaticPayout); 
@@ -70,12 +69,16 @@ contract swapPoolMATICLINK {
         return (poolLinkBalance()-(poolConstantProduct())/(poolMaticBalance()+payMatic ) ); //msg.value updates balance already. 
     }
 
-    function linkforMaticBalanced(uint payLink) public view returns(bool)  {
-        return payLink*maticToReceive(payLink) == poolConstantProduct(); //msg.value updates balance already. 
+    function linkToReceiveMaticReceived(uint payMatic) public view returns(uint)  {
+        return poolConstantProduct()/((poolMaticBalance()-payMatic)*poolLinkBalance()); //msg.value updates balance already. 
     }
 
-    function maticforLinkBalanced(uint payMatic) public view returns(bool)  {
-        return poolMaticBalance()*linkToReceive(0) == poolConstantProduct(); //msg.value updates balance already. 
-    }
+    // function linkforMaticBalanced(uint payLink) public view returns(bool)  {
+    //     return payLink*maticToReceive(payLink) == poolConstantProduct(); //msg.value updates balance already. 
+    // }
+
+    // function maticforLinkBalanced(uint payMatic) public view returns(bool)  {
+    //     return payMatic*linkToReceive(0) == poolConstantProduct(); //msg.value updates balance already. 
+    // }
 
 }
