6b51e786576832cfd7a9c57d2da096b4df74fdf0
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-07-07 00:21:04 -0400

    Update WIP_AMM_DEX.sol

--- Contracts/WIP_AMM_DEX.sol
+++ Contracts/WIP_AMM_DEX.sol
@@ -5,12 +5,26 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ERC20TokenContract is ERC20('Chainlink', 'LINK') {}
 
+
+contract TruckStopToken is ERC20{ //100 Quattuorvigintillion (10**77) is largest possible supply but not practical for trading [too hard to chart].
+
+    address immutable Owner; // Will add TST to Uniswap pool for trading. Largest supply token at this moment is ELON about 1 Quadrillion (10**15)
+
+    constructor() ERC20("TruckStopToken","TST") { //Fair ICOs like ETH have 80% public sale [We have 82%]. 1 Sextillion tokens (10**21).
+        Owner = msg.sender;                                  //Contract deployer is the owner. 
+        _mint(Owner,                        (88)*(10**37) ); //Owner will keep 6 but put 82 on Uniswap.
+    }
+    
+}
+
 contract swapPoolMATICLINK {
     
     address public immutable Owner;
     
-    // ERC20TokenContract tokenObject = ERC20TokenContract(0x326C977E6efc84E512bB9C30f76E30c160eD06FB); //Chainlink contract address on Polygon testnet.
-    ERC20TokenContract tokenObject = ERC20TokenContract(0xfe4F5145f6e09952a5ba9e956ED0C25e3Fa4c7F1); //ERC20 contract address on Polygon testnet.
+    //ERC20TokenContract tokenObject = ERC20TokenContract(0x326C977E6efc84E512bB9C30f76E30c160eD06FB); //Chainlink contract address on Polygon testnet.
+    ERC20TokenContract tokenObject = ERC20TokenContract(0xd9145CCE52D386f254917e481eB44e9943F39138); //Chainlink contract address on Polygon testnet.
+    uint public constantProduct;
+
 
     constructor() {
         Owner = msg.sender;
@@ -22,31 +36,30 @@ contract swapPoolMATICLINK {
     }
 
     modifier poolExists() {
-        require(poolConstantProduct() > 0 , "Pool does not exist yet.");
+        require(constantProduct > 0 , "Pool does not exist yet.");
         _;
     }
 
     function createMaticLinkPool(uint linkDeposit) public payable senderIsOwner {     //NEED TO APPROVE EVERY TIME BEFORE YOU SEND LINK FROM THE ERC20 CONTRACT!
-        require(poolConstantProduct() == 0, "Pool already created.");
+        require(constantProduct == 0, "Pool already created.");
         require(msg.value*linkDeposit > 0, "Matic*Link deposit should be greater than 0!");
         tokenObject.transferFrom(Owner, address(this), linkDeposit); //NEED TO APPROVE EVERY TIME BEFORE YOU SEND LINK FROM THE ERC20 CONTRACT!
+        constantProduct = poolMaticBalance()*poolLinkBalance();
     }
     
     function ownerWithdrawPool() public senderIsOwner poolExists  {
         tokenObject.transfer(Owner, poolLinkBalance());
+        constantProduct = 0;
         payable(Owner).transfer(address(this).balance);
     }
 
     function swapMATICforLINK() public payable poolExists {
-        // require(maticforLinkBalanced(msg.value), "Pool not balanced!");
-        tokenObject.transfer(msg.sender, linkToReceiveMaticReceived(msg.value) ); 
+        tokenObject.transfer(msg.sender, linkToReceiveMaticReceived() ); 
     }
     
     function swapLINKforMATIC(uint payLink) public poolExists {     //NEED TO APPROVE EVERY TIME BEFORE YOU SEND LINK FROM THE ERC20 CONTRACT!
-        // require(linkforMaticBalanced(payLink), "Pool not balanced!");
-        uint memoryMaticPayout = maticToReceive(payLink);
         tokenObject.transferFrom(msg.sender, address(this),  payLink ); 
-        payable(msg.sender).transfer(memoryMaticPayout); 
+        payable(msg.sender).transfer(maticToReceiveLinkReceived()); 
     }    
 
     function poolMaticBalance() public view returns(uint)  {
@@ -57,28 +70,20 @@ contract swapPoolMATICLINK {
         return tokenObject.balanceOf(address(this));
     }
 
-    function poolConstantProduct() public view returns(uint)  {
-        return poolMaticBalance()*poolLinkBalance();
+    function maticToReceive(uint payLink) public view returns(uint)  {
+        return (poolMaticBalance()-(constantProduct)/(poolLinkBalance()+payLink));
     }
 
-    function maticToReceive(uint payLink) public view returns(uint)  {
-        return (poolMaticBalance()-(poolConstantProduct())/(poolLinkBalance()+payLink));
+    function maticToReceiveLinkReceived() public view returns(uint)  {
+        return poolMaticBalance()-(constantProduct/((poolLinkBalance())) ) ; //msg.value updates balance already. 
     }
 
     function linkToReceive(uint payMatic) public view returns(uint)  {
-        return (poolLinkBalance()-(poolConstantProduct())/(poolMaticBalance()+payMatic ) ); //msg.value updates balance already. 
+        return poolLinkBalance()-(constantProduct)/(poolMaticBalance()+payMatic); //msg.value updates balance already. 
     }
 
-    function linkToReceiveMaticReceived(uint payMatic) public view returns(uint)  {
-        return poolConstantProduct()/((poolMaticBalance()-payMatic)*poolLinkBalance()); //msg.value updates balance already. 
+    function linkToReceiveMaticReceived() public view returns(uint)  {
+        return poolLinkBalance()-(constantProduct/((poolMaticBalance())) ) ; //msg.value updates balance already. 
     }
-
-    // function linkforMaticBalanced(uint payLink) public view returns(bool)  {
-    //     return payLink*maticToReceive(payLink) == poolConstantProduct(); //msg.value updates balance already. 
-    // }
-
-    // function maticforLinkBalanced(uint payMatic) public view returns(bool)  {
-    //     return payMatic*linkToReceive(0) == poolConstantProduct(); //msg.value updates balance already. 
-    // }
 
 }
